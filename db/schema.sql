d062370ccb9736c15693e090b685d3df73c871b9
unknown <mlinback83@gmail.com>
2019-02-21 14:48:42 -0700

    initial upload

---
+++ db/schema.sql
@@ -0,0 +1,26 @@
+-- #### DB Setup
+
+-- 1. Inside your `burger` directory, create a folder named `db`.
+
+-- 2. In the `db` folder, create a file named `schema.sql`. Write SQL queries this file that do the following:
+
+--    * Create the `burgers_db`.
+--    * Switch to or use the `burgers_db`.
+--    * Create a `burgers` table with these fields:
+    --  * **id**: an auto incrementing int that serves as the primary key.
+    --  * **burger_name**: a string.
+    --  * **devoured**: a boolean.
+
+
+### schema
+
+CREATE DATABASE burgers_db;
+USE burgers_db;
+
+CREATE TABLE burgers
+(
+    id int NOT NULL AUTO_INCREMENT,
+    burger_name VARCHAR (255) NOT NULL,
+    devoured BOOLEAN DEFAULT false,
+    PRIMARY KEY (ID)
+);
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (name) VALUES ('Hamburger');
+INSERT INTO burgers (name) VALUES ('Cheeseburger');
+INSERT INTO burgers (name, devoured) VALUES ('California Burger', true);
+INSERT INTO burgers (name, devoured) VALUES ('Double Double Animal Style', true);
+INSERT INTO burgers (name, devoured) VALUES ('Black n Blue Burger', true);
+INSERT INTO burgers (name) VALUES ('Veggie Burger');
