35fdd2c051bedfab5ff3bcccfe26293b380fe61c
Marissa Linback <43969868+mlinback@users.noreply.github.com>
2019-02-21 23:10:24 -0700

    Add files via upload

--- controllers/burgers_controllers.js
+++ controllers/burgers_controllers.js
@@ -10,11 +10,11 @@ var express = require("express");
 var router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
-var burger = require("../models/burger.js");
+var burger = require("../models/burger");
 
 // Create all our routes and set up logic within those routes where required.
 router.get("/", function(req, res) {
-  burger.all(function(data) {
+  burger.selectAll(function(data) {
     var hbsObject = {
       burgers: data
     };
@@ -23,46 +23,19 @@ router.get("/", function(req, res) {
   });
 });
 
-router.post("/api/burgers", function(req, res) {
-  burger.create([
-    "name", "devoured"
-  ], [
-    req.body.name, req.body.devoured
-  ], function(result) {
-    // Send back the ID of the new quote
-    res.json({ id: result.insertId });
-  });
-});
+//post new burgers
+router.post("/burger/create", function(req, res) {
+  burger.insertOne([req.body.burger_name], function(result){
+    res.redirect("/");
+  })
+})
 
-router.put("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  console.log("condition", condition);
-
-  burger.update({
-    devoured: req.body.devoured
-  }, condition, function(result) {
-    if (result.changedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
-
-router.delete("/api/burgers/:id", function(req, res) {
-  var condition = "id = " + req.params.id;
-
-  burger.delete(condition, function(result) {
-    if (result.affectedRows == 0) {
-      // If no rows were changed, then the ID must not exist, so 404
-      return res.status(404).end();
-    } else {
-      res.status(200).end();
-    }
-  });
-});
+//devours selected burger
+router.post("/burger/eat/:id", functio(req, res) {
+  burger.updateOne([req.params.id], function(result) {
+    res.redirect("/");
+  })
+})
 
 // Export routes for server.js to use.
 module.exports = router;
